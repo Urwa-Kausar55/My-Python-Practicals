49f7218c76cb1a13c635345c93037dc2efde0a5a
Urwa_Kausar <urwakausar55@gmail.com>
2026-03-20 00:43:45 +0500

    Add files via upload

---
+++ Python-Practice(string).py
@@ -0,0 +1,17 @@
+strg="this is urwa's programe"
+print(strg)
+strg2="I want to disply the next sentence in next line.\nthat's why i use this back slash and'n'"
+print(strg2)
+print(len(strg))
+print (strg2[3])
+print(strg2[7:34])
+print(strg[8:len(strg)])
+#OR
+print(strg[8:])
+strg3="UrwaKausar"
+print(strg3[-10:-6])
+print(strg.endswith("e"))
+print(strg.capitalize())
+print(strg.replace("urwa's","mine"))
+print(strg.find("urwa"))
+print(strg2.count("i"))
