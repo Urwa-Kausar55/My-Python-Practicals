db36686b8ba87c32270fb9c7073c6dd46082c58e
Urwa_Kausar <urwakausar55@gmail.com>
2026-03-20 00:49:08 +0500

    Add files via upload

---
+++ Python-Practice(loops).py
@@ -0,0 +1,41 @@
+i=1
+while i<=10:
+    print(i)
+    i+=1
+ ##Assesment time....!!!!
+ #print numbers from 1 to 100
+i=100
+while i>=1:
+     print(i)
+     i-=1
+ #print a multiplication table of n number
+a=int(input("Enter any number"))
+i=1
+while i<=10:
+     b=a*i
+     print(a,"*",i,"=",b)
+     i+=1   
+#print the element of the following list using a loop
+listt=[1,4,9,16,25,36,49,64,81,100]
+i=0
+while i<len(listt):
+    print(listt[i])
+    i+=1
+# search for a number x in this tupple using loop 
+tupple=(1,4,9,16,25,36,49,64,81,100)
+x=int(input("Enter any number from above tupple"))
+i=0
+while i < len(tupple):
+    if(tupple[i] == x):
+        print("found",i)
+    else:
+        print("nothing")
+    i += 1
+        
+        
+
+ 
+
+   
+
+
