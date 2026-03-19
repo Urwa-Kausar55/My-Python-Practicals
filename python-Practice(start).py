23da875329cb330956a9703998407f6a334f2b94
Urwa_Kausar <urwakausar55@gmail.com>
2026-03-20 00:43:13 +0500

    Add files via upload

---
+++ python-Practice(start).py
@@ -0,0 +1,16 @@
+print("hellow","hows the day",23)
+print(78+67)
+name="urwa"
+age=21
+height=5.2
+print(name)
+print(height,"ft")
+print(type(name))
+print(type(age))
+print(type(height))
+# use hash for single line comment
+"""
+use it for multiline comment
+heheh
+its nice
+"""
