ca5ad0e591f8561779029d5546690c5bb8475d78
Urwa_Kausar <urwakausar55@gmail.com>
2026-03-20 00:49:43 +0500

    Add files via upload

---
+++ Python-Practice(functions).py
@@ -0,0 +1,28 @@
+# def sum(a,b):
+#     sum=a+b
+#     print(sum)
+#     return sum  # or return a+b
+
+
+# sum(3,4)
+
+# sum(7,8)
+
+# sum(1,3)
+
+
+#recursion
+
+def show(n):
+    if(n==0):
+        return
+    print(n)
+    show(n-1)
+    print("End")
+
+
+show(5)
+
+
+
+
