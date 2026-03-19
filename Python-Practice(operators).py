40ecf3dd32a55deafd981348d771d7df18d4c8e9
Urwa_Kausar <urwakausar55@gmail.com>
2026-03-20 00:45:04 +0500

    Add files via upload

---
+++ Python-Practice(operators).py
@@ -0,0 +1,32 @@
+#arthematic operators
+a=2
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)#for remainder
+print(a**b)#for power like a^b
+#relational / comparasion operator
+print(a==b)#False
+print(a!=b)#True
+print(a>=b)
+print(a<=b)
+print(a<b)
+print(a>b)
+#assignment operater
+a+=5
+print(a)
+a-=5
+print(a)
+#and likewise all arthematic operator can use in assignment operator
+#logic operator
+print(not False)
+print(not True)
+print(not a>b)
+print("hehe")
+c=True
+d=False
+print(c and d)
+print(c or d)
+print(a==b or a>b)
